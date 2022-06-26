77c33d10c9947dfad958f6ba83372d0eed35b97e
Hideki Yoshizane <hideki.yoshizane04@gmail.com>
2022-06-26 17:48:56 -0300

    Vencedor adicionado

--- src/Copa.java
+++ src/Copa.java
@@ -1,9 +1,14 @@
 
 public class Copa {
 	protected int ano;
+	protected String vencedor;
 	
 	
 	public int getAno() { return this.ano; }
 	
 	public void setAno(int ano) { this.ano = ano; }
+
+	public String getVencedor(){return this.vencedor;}
+
+	public void setVencedor(String vencedor){this.vencedor = vencedor;}
 }
