e1d321882174f14cd46fda0d1da54f05dcf7ff96
Daniel Neiva <neiva.daniel@outlook.com>
2022-06-29 23:12:14 -0300

    Buscar Grupos

--- src/Grupo.java
+++ src/Grupo.java
@@ -1,3 +1,7 @@
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Grupo {
 
@@ -9,5 +13,47 @@ public class Grupo {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public JSONArray getGrupos(){
+		JSONObject gruposRaw;
+		JSONArray grupos = new JSONArray();
+		
+		gruposRaw = Main.db.data.getJSONObject("groups");
+		Iterator<String> keys = gruposRaw.keys();
+		
+		while(keys.hasNext()) {
+			String key = keys.next();
+			if (gruposRaw.get(key) instanceof JSONObject) {
+				JSONObject grupoRaw = gruposRaw.getJSONObject(key);
+				JSONObject grupo = new JSONObject();
+				grupo.put("nome", grupoRaw.get("name"));
+				grupos.put(grupo);
+			}
+		}
+		
+		return grupos;
+		
+	}
+	
+	public void printInfo(String grupo) {
+		System.out.println(grupo);
+		JSONObject gruposRaw;
+		gruposRaw = Main.db.data.getJSONObject("groups");
+		gruposRaw = gruposRaw.getJSONObject(grupo);
+		String leftAlignFormat = "| %-15s | %-4d |%n";
+		String centerAlignFormat = "| %-15s | %-4d |%n";
+		System.out.format("+-----------------+------+%n");
+		System.out.format("|         " + gruposRaw.get("name") + "        |%n");
+		System.out.format("+-----------------+------+%n");
+		System.out.format("|   Seleção   | Pontos   |%n");
+		System.out.format("+-----------------+------+%n");
+		JSONArray times = gruposRaw.getJSONArray("teams");
+		
+		for (int i=0; i<times.length(); i++) {
+			JSONObject time = times.getJSONObject(i);
+			System.out.format(leftAlignFormat, time.get("name"), time.get("points"));
+		}
+		System.out.format("+-----------------+------+%n");
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,59 @@
 /* Decompiler 12ms, total 863ms, lines 12 */
 import java.io.IOException;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Main {
-   public static void main(String[] args) throws IOException {
-      Database db = new Database();
-      db.loadInfo();
-      System.out.println(db.data.getJSONArray("stadiums").getJSONObject(0).get("nearby_airports"));
-   }
+	static Database db = new Database();
+	
+	
+	public static void main(String[] args) throws IOException {
+	   db.loadInfo();
+	   
+
+	   Scanner input = new Scanner(System.in);
+       int opcaoMenu;
+       
+	   do {
+
+     		System.out.println("Sistema de Acompanhamento da Copa do Mundo!\n\n\n");
+     		System.out.println("Menu: ");
+     		System.out.println("1: Selecione um Grupo");
+     		System.out.println("2: Selecione um time");
+     		System.out.println("3: Selecione um estádio");
+     		System.out.println("4: Selecione uma Cidade");
+     		System.out.println("5: Sair");
+     		
+     		opcaoMenu = input.nextInt();
+            input.nextLine();
+            
+            if (opcaoMenu == 1) {
+            	int opcaoGrupo;
+				do{
+	            	System.out.print("Selecione um Grupo: \n");
+	            	Grupo gp = new Grupo();
+	            	JSONArray grupos = gp.getGrupos();
+	            	for (int i = 0; i < grupos.length(); i++) {
+	            		System.out.println((i+1)+" - "+grupos.getJSONObject(i).getString("nome"));
+	            	}
+	            	opcaoGrupo = input.nextInt();
+	            	String gruposString = "abcdefgh";
+					System.out.println();
+					gp.printInfo(gruposString.substring(opcaoGrupo-1, opcaoGrupo));
+					opcaoGrupo=9;
+					opcaoMenu=5;
+//    				System.out.println("1: View team details");
+//    				System.out.println("2: View matches");
+//    				System.out.println("3: View discipline");
+//    				System.out.println("4: View goals");
+//    				System.out.println("5: View All teams");
+//    				System.out.println("6: Exit");
+				}while(opcaoGrupo != 9);
+                input.nextLine();
+                
+            }
+	   }while(opcaoMenu != 5);
+	}
 }
